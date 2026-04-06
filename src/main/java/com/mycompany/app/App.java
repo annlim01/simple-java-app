332eb7799f2ca9c5ce109c7603a3d240ce021ba6
annlim01 <ann.lim@keysight.com>
2026-04-06 16:07:22 +0800

    demo poll scm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modify local project - 3rd changes");
+        System.out.println("Modify local project - 4th changes - demo poll scm");
     }
 
     public String getMessage() {
