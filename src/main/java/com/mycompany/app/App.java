0145148bd695dfa7d3d2f9c9cd72fff6f62ae0ed
annlim01 <ann.lim@keysight.com>
2026-04-06 14:29:08 +0800

    added all project files in first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "I do not love Jenkins";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello there ! Jenkins lab tutorial- second changes");
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
